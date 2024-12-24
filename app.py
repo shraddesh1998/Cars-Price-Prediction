fe22cdada1d47632fbea3fac280a57254c082da5
Shraddhesh Datkhile <107907994+shraddesh1998@users.noreply.github.com>
2024-12-24 11:41:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -64,5 +64,5 @@ def predict():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#if __name__ == '__main__':
+   # app.run(debug=True)
